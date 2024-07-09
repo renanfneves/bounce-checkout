ada6f3e3351442f3a49fd6f909353de729c50b6c
Renan Floriano Neves <renanfneves@gmail.com>
2024-07-09 15:04:12 -0300

    chore: setup ci/cd

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -6,13 +6,13 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   // reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
   },
   webServer: {
-    command: 'pnpm dev -- --port 5173 --mode test',
+    command: 'pnpm dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
